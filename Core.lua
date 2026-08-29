418a1a0607b416ec07d7bd25c459a986b26d00c4
Iurii Lisenkov <71514792+UnknownAlienHuman@users.noreply.github.com>
2026-08-29 05:04:54 -0400

    chore: advance main to 1.1.0-beta.6

--- Core.lua
+++ Core.lua
@@ -13,9 +13,9 @@ addon.modules = addon.modules or {}
 
 local C_AddOns = _G.C_AddOns
 if C_AddOns and C_AddOns.GetAddOnMetadata then
-    addon.version = C_AddOns.GetAddOnMetadata(addon.name, "Version") or "1.1.0-beta.5"
+    addon.version = C_AddOns.GetAddOnMetadata(addon.name, "Version") or "1.1.0-beta.6"
 else
-    addon.version = "1.1.0-beta.5"
+    addon.version = "1.1.0-beta.6"
 end
 
 function addon:RegisterModule(name, module)
